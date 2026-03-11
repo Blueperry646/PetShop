7d6036e1e50499d3d8ee79395e8329b63b02831f
Blueperry646 <CG3037461@car.ifsp>
2026-03-11 08:43:46 -0300

    iniciando views com mustache

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,15 @@
 import {Request, Response} from 'express';
+import { title } from 'process';
 
 export const home = (req: Request, res: Response) => {
-    res.render('home');
+    // res.render('home');
+    res.render('pages/page', {
+        banner: {
+            title: 'todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
+
 };
 export const cats = (req: Request, res: Response) => {
     res.render('cats');

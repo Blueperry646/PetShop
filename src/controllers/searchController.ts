617a53603a0ede1d42a32511dfc71f6fa670c85e
Blueperry646 <yanmpenteado@gmail.com>
2026-03-30 08:19:47 -0300

    Projeto Consertado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,17 +1,19 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express'
 import { Pet } from '../models/pet'
-import { createMenuObejct } from '../helpers/createMenuObject';
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const search = (req: Request, res: Response) => {
-    let query: string = req.query.q as string 
-    if(!query) {
+    let query: string = req.query.q as string
+    if (!query) {
         res.redirect('/')
         return
-    } 
+    }
+
     let list = Pet.getFromName(query)
-    res.render('pages/pages', {
-        menu: createMenuObejct(''),
+    res.render('pages/page', {
+        menu: createMenuObject(''),
         list,
         query
-    });
-                       }
+    })
+
+}
